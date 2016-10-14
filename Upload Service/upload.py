7ea1de97af8baea262803158a760ce721991f65c
Zaid Mullins <zmullins@onshift.com>
2016-10-14 15:31:59 -0400

    Frontend react stubs

--- Upload Service/upload.py
+++ Upload Service/upload.py
@@ -1,9 +1,25 @@
-from flask import Flask
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/images/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/upload_image")
-def hello():
-    return "Hello World!"
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-if __name__ == "__main__":
+@app.route("/upload_image", methods=['POST'])
+def handle_upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+if __name__ == '__main__':
     app.run()
