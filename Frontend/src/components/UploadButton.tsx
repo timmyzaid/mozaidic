f5d23144f2277a3815d9c1c296e17385c4172c57
Zaid Mullins <zmullins@onshift.com>
2016-12-16 16:05:19 -0500

    Stub for uploading image

--- Frontend/src/components/UploadButton.tsx
+++ Frontend/src/components/UploadButton.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
 
 
+
+function handleUpload(event: React.FormEvent<HTMLInputElement>) {
+    console.log(event);
+
+    let request = new XMLHttpRequest();
+    request.onerror = (ev: ErrorEvent) => {
+        console.log(ev);
+    };
+    request.onload = (ev: Event) => {
+        console.log(ev);
+    };
+    request.open('POST', '/upload_image');
+    request.send();
+}
+
 export function UploadButton({containerClass, labelClass, inputClass, buttonText}: UploadButtonProps) {
     return (
         <div className={containerClass}>
             <label htmlFor="file-upload" className={labelClass}>{buttonText}</label>
-            <input id="file-upload" className={inputClass} type="file"/>
+            <input id="file-upload" className={inputClass} type="file" onChange={handleUpload} />
         </div>
     );
 }
